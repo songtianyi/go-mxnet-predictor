b73d358d3391100640a93f05c359694c1f7d3760
songtianyi <songtianyi630@163.com>
2016-12-18 13:09:04 +0000

    add example jpg;use bild instead of opencv

--- examples/predict.go
+++ examples/predict.go
@@ -1,34 +1,58 @@
 package main
 import (
 	"fmt"
-	"image"
-	"os"
-	"io"
-	"github.com/disintegration/imaging"
+	//"image"
+	//"os"
+	//"io"
+	//"io/ioutil"
+	//"github.com/lazywei/go-opencv/opencv"
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/anthonynsimon/bild/transform"
+	"github.com/anthonynsimon/bild/blend"
+	//"github.com/songtianyi/go-mxnet-predictor"
 )
 
 func main() {
-	reader, err := os.Open("test.jpg")
-	if err != nil {
-		panic(err)
-	}
-	img, _, _ := image.Decode(reader)
-	img = imaging.Fill(img, 224, 224, imagine.Center, imaging.Lanczos)
-	symbol, err := ioutil.ReadFile("./Inception-symbol.json")
-	if err != nil {
-		panic(err)
-	}
-	params, err := ioutil.ReadFile("./Inception-0009.params")
-	if err != nil {
-		panic(err)
-	}
-	synset, err := os.Open("./synset.txt")
-	if err != nil {
-		panic(err)
-	}
+	filePath := "flowertest.jpg"
+	//img := opencv.LoadImage(filePath)
+	//fmt.Println(img.Channels(), img.Depth(), img.Origin, img.Width(), img.Height())
+	//defer img.Release()
+	//dst := opencv.CreateImage(
+	//	img.Width(),
+	//	img.Height(),
+	//	img.Depth(), img.Channels())
+	//defer dst.Release()
+	//opencv.CvtColor(img, dst, 4)
+	//resized := opencv.Resize(dst, 299, 299, opencv.CV_INTER_LINEAR)
+	//defer resized.Release()
 
-	p, err := mxnet.CreatePreditor(symbol, params, mxnet.Device{mxnet.CPU_DEVICE, 0}, []mxnet.InputNode{{Key: "data", Shape: []uint32{1, 3, 224, 224}}})
+	//m := resized.GetMat()
+	//fmt.Println(m.GetData(), m.Rows(), m.Cols(), resized.Width(), resized.Height(), m.Step(), len(m.GetData()))
+	
+img, err := imgio.Open(filePath)
     if err != nil {
-		panic(err)
-	}
+        panic(err)
+    }
+
+    resized := transform.Resize(img, 299, 299, transform.Linear)	
+	// blend.Subtract()
+	
+
+	//symbol, err := ioutil.ReadFile("./Inception-symbol.json")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//params, err := ioutil.ReadFile("./Inception-0009.params")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//synset, err := os.Open("./synset.txt")
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//p, err := mxnet.CreatePredictor(symbol, params, &mxnet.Device{mxnet.CPU_DEVICE, 0}, []mxnet.InputNode{{Key: "data", Shape: []uint32{1, 3, 299, 299}}})
+    	//if err != nil {
+	//	panic(err)
+	//}
 }
