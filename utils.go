b995572d38cbe829eb906e1290aae1bb6fc8e54e
songtianyi <songtianyi630@163.com>
2016-12-18 13:11:52 +0000

    add GetLastError binding

--- utils.go
+++ utils.go
@@ -1,1 +1,20 @@
 package mxnet
+
+/*
+// go preamble
+#cgo pkg-config: mxnet
+#include <mxnet/c_predict_api.h>
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"fmt"
+)
+
+func GetLastError() error {
+        if err := C.MXGetLastError(); err != nil {
+                return fmt.Errorf(C.GoString(err))
+        }
+        return nil
+}
+
