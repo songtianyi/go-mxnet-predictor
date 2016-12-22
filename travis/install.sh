aa139959af1b8ecae8a25e150e4bb661f1a79a72
songtianyi <songtianyi630@163.com>
2016-12-22 05:39:04 +0000

    fix travis

--- travis/install.sh
+++ travis/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ls
 cd travis
 git clone https://github.com/dmlc/mxnet.git --recursive
-cd mxnet/make -j4
+cd mxnet && make -j4
+ln -s lib/libmxnet.so /usr/lib/libmxnet.so
 go get github.com/anthonynsimon/bild
 go get github.com/songtianyi/go-mxnet-predictor
+exit 0
