d97490ec291804afbe348b93fcdf81bc2d01d4c9
songtianyi <songtianyi630@163.com>
2016-12-22 05:54:17 +0000

    fix travis

--- travis/install.sh
+++ travis/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-cd travis
-git clone https://github.com/dmlc/mxnet.git --recursive
-cd mxnet && make -j4
-ln -s lib/libmxnet.so /usr/lib/libmxnet.so
+MY_PATH="`dirname \"$0\"`"  # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`" # absolutized and normalized
+
+cd $MY_PATH/travis && git clone https://github.com/dmlc/mxnet.git --recursive
+cd $MY_PATH/travis/mxnet && make -j4
+ln -s $MY_PATH/travis/lib/libmxnet.so /usr/lib/libmxnet.so
 go get github.com/anthonynsimon/bild
-go get github.com/songtianyi/go-mxnet-predictor
+sed -i "s/MXNET_SRC_DIR/$MY_PATH\/travis\/mxnet/g" $MY_PATH/tools/mxnet.pc
 exit 0
