de5df04499d3e5916146e69438534e0df94be54e
songtianyi <songtianyi630@163.com>
2016-12-22 06:03:41 +0000

    fix travis

--- travis/env.sh
+++ travis/env.sh
@@ -1,5 +1,6 @@
 # script to be sourced in travis yml
 # setup all enviroment variables
+set -x
 export LIBRARY_PATH=$LIBRARY_PATH:${PWD}/travis/mxnet/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/travis/mxnet/lib/
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${PWD}/tools/
