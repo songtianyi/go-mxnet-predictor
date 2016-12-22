085d5dff9fe98e27aa6e19cf8413930ba4ca53e1
songtianyi <songtianyi630@163.com>
2016-12-22 06:15:20 +0000

    fix travis

--- travis/env.sh
+++ travis/env.sh
@@ -1,7 +1,5 @@
 # script to be sourced in travis yml
 # setup all enviroment variables
-set -x
 export LIBRARY_PATH=$LIBRARY_PATH:${PWD}/travis/mxnet/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/travis/mxnet/lib/
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${PWD}/tools/
 
--- travis/install.sh
+++ travis/install.sh
@@ -3,7 +3,8 @@ set -x
 MY_PATH="`dirname \"$0\"`"  # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`" # absolutized and normalized
 
-checkRet(ret) {
+checkRet() {
+	ret=$1
 	if [[ $ret -eq 0 ]];then
 		return
 	else
@@ -13,12 +14,13 @@ checkRet(ret) {
 
 cd $MY_PATH/travis && git clone https://github.com/dmlc/mxnet.git --recursive
 cd $MY_PATH/travis/mxnet && make -j4
-checkRet $0
+checkRet $?
 
 go get github.com/anthonynsimon/bild
 
 sed -i "s/MXNET_SRC_DIR/$MY_PATH\/travis\/mxnet/g" $MY_PATH/tools/mxnet.pc
+sudo cp $MY_PATH/tools/mxnet.pc /usr/local/lib/pkgconfig/
 pkg-config --libs mxnet
-checkRet $0
+checkRet $?
 
 exit 0
